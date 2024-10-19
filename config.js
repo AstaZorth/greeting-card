c7a789396e0de6133cd2ffb52e6541b88469b280
AstaZorth <phuongtrumnewbie357@gmail.com>
2024-10-20 05:10:35 +0700

    Add files via upload
    .

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Mẹ';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Ngày 20/10, con không biết nói gì ngoài gửi đến mẹ lời chúc mạnh khỏe, trẻ mãi không già, luôn là người phụ nữ xinh đẹp nhất trong lòng con. Dù nhiều lúc con hay cãi lời mẹ, nhưng với con, mẹ vẫn luôn là người quan trọng nhất.';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
